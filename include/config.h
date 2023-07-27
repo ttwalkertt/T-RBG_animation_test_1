0c83ccc25e6ba6fd86641c7233f6f89e87454313
Tim Walker <timtwalk@amazon.com>
2023-07-26 21:32:13 -0600

    initial

---
+++ include/config.h
@@ -0,0 +1,5 @@
+#define MY_SSID "walker"
+#define MY_PASSWORD "3037753770"
+#define MY_TIMEZONE -8
+
+#define VERBOSE 2
---
+++ include/ui.h
@@ -0,0 +1,22 @@
+// ui.h for the ui class
+
+class UI {
+    public:
+        // Constructor
+        UI();
+
+        // Destructor
+        ~UI();
+
+    //     // Getter methods
+    //     std::string getName() const;
+    //     int getAge() const;
+
+    //     // Setter methods
+    //     void setName(const std::string& name);
+    //     void setAge(int age);
+
+    // private:
+    //     std::string name;
+    //     int age;
+};
